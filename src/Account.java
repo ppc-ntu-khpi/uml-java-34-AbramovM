d8d5848454ed5b0b78bf2421b701c61a6fa37b3d
AbramovM <62811238+AbramovM@users.noreply.github.com>
2020-05-06 20:55:20 +0300

    Add files via upload

---
+++ src/Account.java
@@ -0,0 +1,28 @@
+package javabank;
+
+public class Account {
+
+    private int number;
+
+    private int balance;
+
+    private float deposite;
+
+    private float withdraw;
+
+    public int getNumber() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public int getBalance() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public float setWithdraw(float withdraw) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public float setDeposite(float deposite) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ src/Bank.java
@@ -0,0 +1,25 @@
+package javabank;
+
+import java.util.List;
+public class Bank {
+
+    private String name;
+
+    private int idBank;
+
+    private List <Account> accounts;
+
+    private List <BankEmployee> bankEmloyee;
+
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public BankEmployee addBankEmployee(BankEmployee bankEmployee) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public BankEmployee removeBankEmployee(BankEmployee bankEmployee) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
